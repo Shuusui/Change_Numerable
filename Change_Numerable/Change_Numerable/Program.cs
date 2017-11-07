c403864814de36f8d8c50b4114d326379168d255
Unknown <K.schaefer92@gmx.de>
2017-11-07 14:07:05 +0100

    Some changes, not finished

--- Change_Numerable/Change_Numerable/Program.cs
+++ Change_Numerable/Change_Numerable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,69 @@ namespace Change_Numerable
     {
         static void Main(string[] args)
         {
+            foreach (var l in System.IO.File.ReadAllLines(@"..\..\Program.cs").Every2nd())
+            {
+                Console.WriteLine(l); 
+            }
+        }
+    }
 
 
+    public static class StringExtensions
+    {
+        public static IEnumerable<string> Every2nd(this IEnumerable<string> str)
+        {
+
+            return str;
+        }
+    }
+    
+    class ExtenededEnumerable : IEnumerable<string>
+    {
+        private IEnumerator<string> str;
+
+        ExtenededEnumerable(IEnumerator<string> str)
+        {
+            this.str = str; 
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            ExtendedEnumerator numerator = new ExtendedEnumerator(str); 
+            return numerator; 
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() //egal
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    class ExtendedEnumerator : IEnumerator<string>
+    {
+        public string Current => throw new NotImplementedException();
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public ExtendedEnumerator(IEnumerator<string> str)
+        {
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
         }
     }
 }
